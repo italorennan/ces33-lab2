0ad070a6f0c508c62fefc9eea0174293a7db9814
Italo Rennan <italorennanls@gmail.com>
2020-06-10 20:46:13 -0300

    Criptografia de uma entrada de dados

--- matrixMultiplication.cpp
+++ matrixMultiplication.cpp
@@ -1,46 +1,70 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <cmath>
 #include <pthread.h>
 
 using namespace std;
 
 // Tamanho da matriz codificadora
-#define SIZE 4
+//#define SIZE 4
 
-// Matriz codificadora
-// Atual = ENGEN COMPUTACAO
-// Futuramente: criada a partir de palavra inserida
-double cod[SIZE][SIZE] = {
-    {69, 78, 71, 69},
-    {78, 32, 67, 79},
-    {77, 80, 85, 84},
-    {65, 67, 65, 79}
-};
-// Matriz decodificadora = cod^-1
-// Futuramente: calcular matriz inversa
-double decod[SIZE][SIZE] = {
-    {15103.0/136453.0, 14603.0/409359.0, -42418.0/409359.0, -9074.0/409359.0},
-    {5023.0/136453.0, -6482.0/409359.0, -12680.0/409359.0, 6803.0/409359.0},
-    {-10267.0/136453.0, -1225.0/136453.0, 17939.0/136453.0, -8882.0/136453.0},
-    {-8239.0/136453.0, -3494.0/409359.0, 1375.0/409359.0, 28802.0/409359.0}
-};
+// Informacoes das matrizes de criptografia
+int SIZE = 0; // Dimensões da matriz
 int step = 0; // Variavel para linha atual
+double** cod; // Matriz codificadora
+double** decod; // Matriz decodificadora
 
 // Informacoes do dado analisado
-double* input[SIZE];
-double* output[SIZE];
+double** input;
+double** output;
+double** output2;
 string info;
 int columns = 0;
 
 // Matrizes da multiplicação: A * B = C
 struct thread_data {
-  double** A, B, C;  
+  double** A;
+  double** B;
+  double** C;  
 };
 
-// Mutex e threads para analise da linha a ser calculada
+// Mutex para analise da linha a ser calculada
 pthread_mutex_t mutex;
-pthread_t threads[SIZE];
+
+void defineCripto () {
+    // Alocacao e definicao das matrizes de criptografia
+    // Futuramente: feito em funcao separada, matrizes nao fixas
+    SIZE = 4;
+    cod = new double*[SIZE];
+    decod = new double*[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        cod[i] = new double[SIZE];
+        decod[i] = new double[SIZE];
+    }
+
+    // Usando tabelas auxiliares pq n sei fazer direto com os ponteiros
+    // E ja suguei tentando descobrir fodase
+    double auxCod[SIZE][SIZE] = {
+        {69, 78, 71, 69},
+        {78, 32, 67, 79},
+        {77, 80, 85, 84},
+        {65, 67, 65, 79}
+    };
+
+    double auxDecod[SIZE][SIZE] = {
+        {15103.0/136453.0, 14603.0/409359.0, -42418.0/409359.0, -9074.0/409359.0},
+        {5023.0/136453.0, -6482.0/409359.0, -12680.0/409359.0, 6803.0/409359.0},
+        {-10267.0/136453.0, -1225.0/136453.0, 17939.0/136453.0, -8882.0/136453.0},
+        {-8239.0/136453.0, -3494.0/409359.0, 1375.0/409359.0, 28802.0/409359.0}
+    };
+
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++) {
+            cod[i][j] = auxCod[i][j];
+            decod[i][j] = auxDecod[i][j];
+        }
+}
 
 void read () {
     // Ler dado
@@ -50,17 +74,22 @@ void read () {
     // Determinar tamanho do dado e da sua matriz
     int length = info.size();
     columns = length / SIZE;
-    int remainder = length % SIZE;
-    if (remainder != 0) {
+    if (length % SIZE != 0) {
         columns++;
+        // Preenchimento de espaços vazios usados na matriz
+        int remainder = SIZE * columns - length;
         for (int i = 0; i < remainder; i++)
-            info[length + i] = char(0);
+            info[length + i] = char(1);
     }
 
     // Alocar espaço de memória para matrizes
+    input = new double*[SIZE];
+    output = new double*[SIZE];
+    output2 = new double*[SIZE];
     for (int i = 0; i < SIZE ; i++) {
         input[i] = new double[columns];
         output[i] = new double[columns];
+        output2[i] = new double[columns];
     }
 
     // Gerar matriz correspondente ao dado (caracteres em ASCII)
@@ -76,9 +105,20 @@ void read () {
     }
 }
 
+// Funcao para escrever dado correspondente a uma matriz
+void write (double** M) {
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < columns; j++) {
+            int c = round(M[i][j]);
+            if (c == 1) return;
+            cout << char(c);
+        }
+}
+
+// Funcao de multiplicação entre duas matrizes referenciadas no argumento
 void* mult (void* arg) {
-    /*struct thread_data *this_data;
-    this_data = (struct thread_data *) arg;*/
+    struct thread_data *this_data;
+    this_data = (struct thread_data *) arg;
 
     // Determinar linha calculada: região critica
     pthread_mutex_lock(&mutex);
@@ -89,11 +129,12 @@ void* mult (void* arg) {
     // Calcular cada elemento da linha
     for (int i = 0; i < columns; i++)
         for (int j = 0; j < SIZE; j++)
-            output[core][i] += cod[core][j] * input[j][i];
-            //(this_data->C)[core][i] += (this_data->A)[core][j] * (this_data->B)[j][i];
+            (this_data->C)[core][i] += (this_data->A)[core][j] * (this_data->B)[j][i];
 }
 
 int main () {
+    defineCripto();
+
     // Exibir matriz cod e decod
     cout << "Matriz cod" << endl;
     for (int i = 0; i < SIZE; i++) {
@@ -118,31 +159,62 @@ int main () {
     // Leitura do dado a ser codificado
     read();
 
-    // Criacao das threads para cada linha
-    /*struct thread_data td;
-    *td.A = *cod;
-    td.B = **input;
-    td.C = **output;*/
+    // Criacao das threads para cada linha para codificar
+    struct thread_data td;
+    td.A = cod;
+    td.B = input;
+    td.C = output;
+
+    pthread_t threads[SIZE];
 
     for (int i = 0; i < SIZE; i++) 
-        pthread_create(&threads[i], NULL, mult, NULL);
-        //pthread_create(&threads[i], NULL, mult, (void *)&td);
+        pthread_create(&threads[i], NULL, mult, (void *)&td);
 
     for (int i = 0; i < SIZE; i++)
         pthread_join(threads[i], NULL);
 
-    cout << endl << "Matriz resultado:" << endl;
+    cout << endl << "Matriz codificada:" << endl;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < columns; j++)
             cout << output[i][j] << " ";
         cout << endl;
     }
 
+    // Criacao das threads para cada linha para decodificar
+    td.A = decod;
+    td.B = output;
+    td.C = output2;
+
+    for (int i = 0; i < SIZE; i++)
+        pthread_create(&threads[i], NULL, mult, (void *)&td);
+
+    for (int i = 0; i < SIZE; i++)
+        pthread_join(threads[i], NULL);
+
+    cout << endl << "Matriz decodificada:" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < columns; j++)
+            cout << output2[i][j] << " ";
+        cout << endl;
+    }
+
+    cout << "Escrevendo o dado decodificado..." << endl;
+    write(output2);
+
     // Liberar espaço alocado para as matrizes
     for (int i = 0; i < SIZE; i++) {
+        delete[] cod[i];
+        delete[] decod[i];
         delete[] input[i];
         delete[] output[i];
+        delete[] output2[i];
     }
+
+    delete[] cod;
+    delete[] decod;
+    delete[] input;
+    delete[] output;
+    delete[] output2;
 
     // Destruir o mutex
     pthread_mutex_destroy(&mutex);
